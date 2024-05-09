26405c199b3827afda58510f071f2c031534abb0
MisterBrash <matthew.brash@ocsb.ca>
2024-05-09 15:08:45 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+/**
+ *  ICS4UC - Mr. Brash 🐿️
+ *  Library of functions
+ *
+ *  This is an example of loading and using the Library
+ **/
+
+"use strict";
+
+// Load the Library
+const Library = require("./library.js");
+
+// Create an array of integers
+let large_array = Library.arrayOfInts(50000, -99999, 99999);
